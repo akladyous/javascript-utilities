d0f0c6d177a106e5ec377367cbab60defa798936
Boula Akladyous <akladyous@gmail.com>
2022-12-25 11:40:47 -0500

    capitalize function

--- utility/utility.js
+++ utility/utility.js
@@ -1,20 +1,5 @@
 export const camelCaseToString = (str) => str.split(/(?=[A-Z])/).join(" ");
 
-export default (() => {
-  Object.defineProperty(String, "capitalize", {
-    value: function () {
-      return this.charAt(0).toUpperCase().concat(this.slice(1));
-    },
-  });
-})();
-
-Array.prototype.random = function () {
-  return this[Math.floor(Math.random() * this.length)];
-};
-export function getImageUrl(name) {
-  return new URL(name, import.meta.url).href;
-}
-
 export function $() {
   const self = {
     string: {
